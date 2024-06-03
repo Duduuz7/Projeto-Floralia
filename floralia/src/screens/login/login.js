2fcba0a130571aa299c0fc74e71b110c03b8e3ba
Guilherme Luiz Da Silva <36992072854@info.local>
2024-06-03 15:06:07 -0300

    att

--- floralia/src/screens/login/login.js
+++ floralia/src/screens/login/login.js
@@ -1,9 +1,9 @@
 import { TouchableOpacity } from "react-native"
-import { Container } from "../components/container/style"
-import { InputLinhaVerde } from "../components/input/style"
-import { LinkRosa, TextInputVerde } from "../components/text/style"
-import { TitleVerde } from "../components/title/style"
-import { ButtonVerde } from "../components/button/style"
+import { Container } from "../../components/container/style"
+import { InputLinhaVerde } from "../../components/input/style"
+import { LinkRosa, TextInputVerde } from "../../components/text/style"
+import { TitleVerde } from "../../components/title/style"
+import { ButtonVerde } from "../../components/button/style"
 
 export const Login = () => {
     return(
