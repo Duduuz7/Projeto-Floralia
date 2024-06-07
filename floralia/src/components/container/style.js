ec498b3ec95bba260d46b6964621aac739d005ba
Guilherme Luiz Da Silva <36992072854@info.local>
2024-06-07 16:05:52 -0300

    inputErro

--- floralia/src/components/container/style.js
+++ floralia/src/components/container/style.js
@@ -6,3 +6,11 @@ export const Container = styled.SafeAreaView`
     background-color: #fafafa;
     gap: 5px;
 `
+
+export const ContainerSecure = styled.View`
+    width: 100%;
+    height: 90px;
+    margin-left: 10%;
+    margin-bottom: 18px;
+    margin-top: -25px;
+`
--- floralia/src/components/input/style.js
+++ floralia/src/components/input/style.js
@@ -17,3 +17,21 @@ export const InputLinhaVerde = styled.TextInput.attrs({
     font-family: "Exo2_600SemiBold";
     color: #386E17;
 `
+
+export const InputErro = styled.TextInput.attrs({
+    placeholderTextColor: "#FF0000"
+})`
+    width: 90%;
+    height: 51px;
+    padding: 10px 40px 10px 10px;
+    margin-top: 15px;
+    background-color: "F6F6F6";
+
+    border-bottom-width: 2px;
+    border-bottom-color: #FF0000;
+    border-radius: 5px;
+
+    font-size: 14px;
+    font-family: "Exo2_600SemiBold";
+    color: #FF0000;
+`
