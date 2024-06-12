b7a354f803cf2cb8322a662e1d7bc58a835da766
Guilherme Luiz Da Silva <36992072854@info.local>
2024-06-12 15:13:43 -0300

    tentativa da camera

--- floralia/src/utils/Auth.js
+++ floralia/src/utils/Auth.js
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { jwtDecode } from "jwt-decode";
-import { encode, decode } from "base-64";
+// import { encode, decode } from "base-64";
 
 export const userDecodeToken = async () => {
     const token = JSON.parse(await AsyncStorage.getItem("token")).token;
